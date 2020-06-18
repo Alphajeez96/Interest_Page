cb9143de636a356605ff6aca8bac3ce79a1ed311
Prince <chimeremezechukwudire@gmail.com>
2020-06-18 11:39:30 +0100

    interest script added

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,28 +1,45 @@
 let principal = 0;
-let interestRate = 10;
-let term = 0;
+let interestRate = 0;
+// let term = 0;
 let timesCompounded = 0;
 
 let amount = 0;
+
+const selectday = () => {
+    let e = document.getElementById("ddlViewBy");
+    let strUser = e.options[e.selectedIndex].text;
+    document.getElementById("days-count").innerHTML = strUser;
+
+    if (strUser === '10 - 30 days') {
+        interestRate = parseFloat(document.getElementById("rate").value = 6 + '%')
+    } else if (strUser === '31 - 60 days') {
+        interestRate = parseFloat(document.getElementById("rate").value = 8 + '%')
+    } else if (strUser === '61 - 90 days') {
+        interestRate = parseFloat(document.getElementById("rate").value = 10 + '%')
+    } else if (strUser === '91days to 2years') {
+        interestRate = parseFloat(document.getElementById("rate").value = 13 + '%')
+    } else if (strUser === 'Over 2 years') {
+        interestRate = parseFloat(document.getElementById("rate").value = 31 + '%')
+    }
+}
 
 const simpleInterest = () => {
     event.preventDefault();
 
     let principal = parseFloat(document.getElementById("amount").value);
-    let interestRate = parseFloat(document.getElementById("rate").value)
+    // let interestRate = parseFloat(document.getElementById("rate").value)
     interestRate = interestRate / 100;
 
-    let term = parseFloat(document.getElementById("term").value);
-    let simpleInt = principal * interestRate * term;
+    // let term = parseFloat(document.getElementById("term").value);
+    let simpleInt = principal * interestRate;
     console.log(simpleInt)
 
     let amount = (principal + simpleInt).toFixed(2);
 
     document.getElementById("intertestearned").value = "₦" + " " + simpleInt.toFixed(2);
-    // document.getElementById("intertestearned").value = simpleInt;
-
     document.getElementById("amountpayable").innerHTML = "₦ " + " " + amount;
     document.getElementById("amountpayables").innerHTML = "₦ " + " " + amount;
+
 
 
 }
